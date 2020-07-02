d3526aa36f85ce41438e6f5e4e82b67e5e878ff7
Paulo Henrique Wilhelms <paulo.wilhelms@hotmail.com>
2020-07-02 18:10:29 -0300

    Update gitignore

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,8 @@
 package applicantion;
 
-import java.util.Scanner;
-
 public class Program {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-
-		sc.close();
 	}
 }
